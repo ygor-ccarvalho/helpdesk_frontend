5905b4e2f3931e3a27ae2e627c3a002931e5d8af
Ygor César de Carvalho <109998784+ygor-ccarvalho@users.noreply.github.com>
2026-04-08 13:30:15 -0300

    Feat: Cria o HTML e Css para o componente nav.

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,16 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
-  selector: 'app-nav',
-  imports: [],
-  templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css'],
+  selector: 'sidenav-content',
+  standalone: true,
+  templateUrl: 'nav.component.html',
+  styleUrls: ['nav.component.css'],
+  imports: [
+    MatSidenavModule,
+    MatButtonModule,
+    MatListModule,
+    MatIconModule
+  ],
 })
-export class NavComponent implements OnInit{
-  
-  constructor() {}
-
-  ngOnInit(): void{
-
-  }
+export class NavComponent {
+  showFiller = false;
 }
