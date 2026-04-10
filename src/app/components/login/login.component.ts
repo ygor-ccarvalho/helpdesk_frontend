f7f9c0d14885bceb943f405c1cd940af554801b6
Ygor César de Carvalho <109998784+ygor-ccarvalho@users.noreply.github.com>
2026-04-10 14:37:07 -0300

    Feat: Cria a validação de formulario para login.

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,14 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormsModule, Validators, ReactiveFormsModule, FormGroup } from '@angular/forms';
+import { Credenciais } from '../../models/credenciais';
 
 @Component({
   selector: 'app-login.component',
-  imports: [FormsModule, MatFormFieldModule, MatInputModule],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent { }
+export class LoginComponent implements OnInit {
+
+  form = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    senha: new FormControl('', [Validators.required, Validators.minLength(3)])
+  });
+
+  constructor() { }
+  ngOnInit(): void { }
+
+  validaCampos(): boolean {
+    return this.form.valid;
+  }
+
+  logar() {
+    const email = this.form.value.email;
+    const senha = this.form.value.senha;
+
+    console.log(email, senha);
+  }
+}
 
 
