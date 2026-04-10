87c9aa39470e22da664d9cfb0deb31796a30374b
Ygor César de Carvalho <109998784+ygor-ccarvalho@users.noreply.github.com>
2026-04-10 12:20:19 -0300

    Feat: Cria o HTML e CSS para o componente Login.

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login.component',
-  imports: [],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent { }
+
+
