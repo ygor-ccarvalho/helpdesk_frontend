1e7f38630b690d39e95ac6191b123a5b1ac9d7e6
Ygor César de Carvalho <109998784+ygor-ccarvalho@users.noreply.github.com>
2026-04-10 11:29:58 -0300

    Feat: Cria a rota para o componente Login.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,13 @@ import { Routes } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component'
 import { HomeComponent } from './components/home/home.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component'
+import { LoginComponent } from './components/login/login.component';
 
 
 export const routes: Routes = [
+    {path: 'login', component: LoginComponent
+
+    },
     {
         path: '',
         component: NavComponent,
