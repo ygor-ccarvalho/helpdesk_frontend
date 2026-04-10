f870fce6281589c7f5543fd2d57e81f6f6263238
Ygor César de Carvalho <109998784+ygor-ccarvalho@users.noreply.github.com>
2026-04-10 11:21:01 -0300

    Feat: Cria a o HTML e CSS para tecnico-list.

--- src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
+++ src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { OnInit, Component, ViewChild } from '@angular/core';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { Tecnico } from '../../../models/tecnico';
 
 @Component({
   selector: 'app-tecnico-list',
   standalone: true,
   templateUrl: './tecnico-list.component.html',
-  styleUrls: ['./tecnico-list.component.css']
+  styleUrls: ['./tecnico-list.component.css'],
+  imports: [MatTableModule, MatPaginatorModule]
 })
-export class TecnicoListComponent {}
+  
+export class TecnicoListComponent implements OnInit {
+
+  ELEMENT_DATA: Tecnico[] = [
+    {
+      id: 1,
+      nome: 'Ygor Carvalho',
+      cpf: '123.456.789-12',
+      email: 'ygor@email.com',
+      senha: '1234',
+      perfis: ['0'],
+      dataCriacao: '15/08/2025'
+
+     }
+  ]
+
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'acoes'];
+  dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  ngOnInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+}
+
