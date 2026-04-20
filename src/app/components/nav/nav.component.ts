7f5fb7b07b954b91d397bc5f3c0a1e51b6272bfd
Ygor César de Carvalho <109998784+ygor-ccarvalho@users.noreply.github.com>
2026-04-20 16:57:52 -0300

    Feat: Cria o HTML e CSS para o componente Tecnico-Create.

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -36,7 +36,7 @@ export class NavComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.router.navigate(['tecnicos'])
+    this.router.navigate(['tecnicos/create'])
   }
 
   logout() {
--- src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
+++ src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-tecnico-create.component',
-  imports: [],
+  imports: [FormsModule, MatCheckboxModule, MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule, MatDividerModule],
   templateUrl: './tecnico-create.component.html',
   styleUrl: './tecnico-create.component.css',
 })
 export class TecnicoCreateComponent {}
+       
