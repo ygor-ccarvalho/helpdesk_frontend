c4e74e7078b1e5302103d1a5b2d38c2968820493
Ygor César de Carvalho <109998784+ygor-ccarvalho@users.noreply.github.com>
2026-04-23 07:10:41 -0300

    Feat: Cria a rota para o componente Chamados.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,10 +11,12 @@ import { ClienteListComponent } from './components/cliente/cliente-list/cliente-
 import { ClienteCreateComponent } from './components/cliente/cliente-create/cliente-create.component';
 import { ClienteUpdateComponent } from './components/cliente/cliente-update/cliente-update.component';
 import { ClienteDeleteComponent } from './components/cliente/cliente-delete/cliente-delete.component';
+import { ChamadoListComponent } from './components/chamado/chamado-list/chamado-list.component';
 
 
 export const routes: Routes = [
-    {path: 'login', component: LoginComponent
+    {
+        path: 'login', component: LoginComponent
 
     },
     {
@@ -28,10 +30,13 @@ export const routes: Routes = [
             { path: 'tecnicos/create', component: TecnicoCreateComponent },
             { path: 'tecnicos/update/:id', component: TecnicoUpdateComponent },
             { path: 'tecnicos/delete/:id', component: TecnicoDeleteComponent },
+
             { path: 'clientes', component: ClienteListComponent },
             { path: 'clientes/create', component: ClienteCreateComponent },
             { path: 'clientes/update/:id', component: ClienteUpdateComponent },
-            { path: 'clientes/delete/:id', component: ClienteDeleteComponent }
+            { path: 'clientes/delete/:id', component: ClienteDeleteComponent },
+
+            { path: 'chamados', component: ChamadoListComponent },
 
         ]
     },
---
+++ src/app/components/chamado/chamado-list/chamado-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chamado-list.component',
+  imports: [],
+  templateUrl: './chamado-list.component.html',
+  styleUrl: './chamado-list.component.css',
+})
+export class ChamadoListComponent {}
