d4291ac8f6863eb6ed2ed18bc5e9fc044f91bd52
Ygor César de Carvalho <109998784+ygor-ccarvalho@users.noreply.github.com>
2026-04-25 09:09:41 -0300

    Feat: Adiciona o HTML e CSS para o componente chamado-create.

--- src/app/components/chamado/chamado-create/chamado-create.component.ts
+++ src/app/components/chamado/chamado-create/chamado-create.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-chamado-create.component',
-  imports: [],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatButtonModule],
   templateUrl: './chamado-create.component.html',
   styleUrl: './chamado-create.component.css',
 })
