b09d25ccf22b0b31c319fcd586b317e7325d111e
Ygor César de Carvalho <109998784+ygor-ccarvalho@users.noreply.github.com>
2026-04-25 10:17:18 -0300

    Feat: Cria validação para formularios do componente chamado-create.

--- src/app/components/chamado/chamado-create/chamado-create.component.ts
+++ src/app/components/chamado/chamado-create/chamado-create.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators, ReactiveFormsModule, FormGroup } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -7,8 +7,30 @@ import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-chamado-create.component',
-  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatButtonModule],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatButtonModule],
   templateUrl: './chamado-create.component.html',
   styleUrl: './chamado-create.component.css',
 })
-export class ChamadoCreateComponent {}
+
+export class ChamadoCreateComponent implements OnInit {
+
+  form = new FormGroup({
+    prioridade: new FormControl(null, [Validators.required]),
+    status: new FormControl(null, [Validators.required]),
+    titulo: new FormControl(null, [Validators.required]),
+    descricao: new FormControl(null, [Validators.required]),
+    tecnico: new FormControl(null, [Validators.required]),
+    cliente: new FormControl(null, [Validators.required]),
+  });
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+  validaCampos(): boolean{
+    return this.form.valid;
+  }
+
+}
