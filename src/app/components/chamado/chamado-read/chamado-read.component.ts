32ca4fab3287315ca17457c4b2808584e35e8ff8
Ygor César de Carvalho <109998784+ygor-ccarvalho@users.noreply.github.com>
2026-04-26 15:04:26 -0300

    Feat: Finaliza configurações para exibição de chamados em chamado-read.

--- src/app/components/chamado/chamado-read/chamado-read.component.ts
+++ src/app/components/chamado/chamado-read/chamado-read.component.ts
@@ -1,9 +1,64 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { Chamado } from '../../../models/chamado';
+import { ChamadoService } from '../../../services/chamado.service';
+import { ChangeDetectorRef } from '@angular/core';
+
 
 @Component({
-  selector: 'app-chamado-read.component',
-  imports: [],
+  selector: 'app-chamado-read',
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatButtonModule, RouterModule, CommonModule],
   templateUrl: './chamado-read.component.html',
   styleUrl: './chamado-read.component.css',
 })
-export class ChamadoReadComponent {}
+export class ChamadoReadComponent implements OnInit {
+
+  chamado: Chamado | null = null;
+
+  constructor(
+    private chamadoService: ChamadoService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private cdr: ChangeDetectorRef 
+  ) { }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+
+    if (id) {
+      this.findById(Number(id));
+    }
+  }
+
+  findById(id: number): void {
+    this.chamadoService.findById(id).subscribe({
+      next: resposta => {
+        this.chamado = resposta;
+        this.cdr.detectChanges();
+      },
+      error: err => console.error('Erro:', err)
+    });
+  }
+
+  retornaStatus(status: number): string {
+    if (status === 0) return 'Aberto';
+    if (status === 1) return 'Em andamento';
+    return 'Encerrado';
+  }
+
+  retornaPrioridade(prioridade: number): string {
+    if (prioridade === 0) return 'Baixa';
+    if (prioridade === 1) return 'Média';
+    return 'Alta';
+  }
+
+  voltar() {
+    this.router.navigate(['chamados']);
+  }
+}
