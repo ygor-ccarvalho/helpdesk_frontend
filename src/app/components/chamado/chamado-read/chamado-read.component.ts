d590804143c03db7ace3cf7e3b56a4e3c4cc3e6b
Ygor César de Carvalho <109998784+ygor-ccarvalho@users.noreply.github.com>
2026-04-26 13:16:49 -0300

    Feat: Cria o componente chamado-read.

---
+++ src/app/components/chamado/chamado-read/chamado-read.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chamado-read.component',
+  imports: [],
+  templateUrl: './chamado-read.component.html',
+  styleUrl: './chamado-read.component.css',
+})
+export class ChamadoReadComponent {}
