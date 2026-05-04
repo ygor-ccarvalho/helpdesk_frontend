d4219b0a64746f4b7c8c88bc0e622fc4ee906230
Ygor César de Carvalho <109998784+ygor-ccarvalho@users.noreply.github.com>
2026-05-04 14:50:01 -0300

    Feat: Refatora o componente login.

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -5,38 +5,51 @@ import { FormControl, FormsModule, Validators, ReactiveFormsModule, FormGroup } 
 import { ToastrService } from 'ngx-toastr';
 import { AuthService } from '../../services/auth.service';
 import { Credenciais } from '../../models/credenciais';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
 
 @Component({
   standalone: true,
-  selector: 'app-login.component',
-  imports: [FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule],
+  selector: 'app-login', 
+  imports: [
+    FormsModule, MatFormFieldModule, MatInputModule,
+    ReactiveFormsModule, MatIconModule, MatProgressSpinnerModule,
+    MatButtonModule, RouterModule, CommonModule
+  ],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+
+  loading = false;      
+  senhaVisivel = false;
 
   form = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
     senha: new FormControl('', [Validators.required, Validators.minLength(3)])
   });
 
+
   constructor(
     private toastr: ToastrService,
     private service: AuthService,
     private router: Router) { }
-  
+
   ngOnInit(): void {
     if (this.service.isAuthenticated()) {
       this.router.navigate(['']);
     }
   }
 
-  validaCampos(): boolean {
-    return this.form.valid;
-  }
-
   logar() {
+    if (this.form.invalid) return;
+
+    this.loading = true; 
+
     const creds: Credenciais = {
       email: this.form.value.email ?? '',
       senha: this.form.value.senha ?? ''
@@ -45,20 +58,19 @@ export class LoginComponent implements OnInit {
     this.service.authenticate(creds).subscribe({
       next: resposta => {
         const authHeader = resposta.headers.get('Authorization');
-
         if (authHeader) {
           this.service.successfullLogin(authHeader);
           this.router.navigate(['']);
         } else {
           this.toastr.error('Token não recebido', 'Erro');
+          this.loading = false;
         }
       },
       error: () => {
         this.toastr.error('Usuário ou senha inválidos!', 'Login');
         this.form.get('senha')?.setValue('');
+        this.loading = false; 
       }
     });
   }
 }
-
-
